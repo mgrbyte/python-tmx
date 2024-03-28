fd6a46cfb172755e760cc304f1b185201bdf4003
Matt Russell <m.russell@bangor.ac.uk>
2024-03-28 10:09:07 +0000

    Forward more references at the top level.
    From tmx.structural:
     - header
     - tu
     - tuv

--- src/tmx/__init__.py
+++ src/tmx/__init__.py
@@ -1,4 +1,4 @@
 from .loaders import load_tmx
-from .structural import tmx
+from .structural import tmx, header, tu, tuv
 
-__all__ = ["load_tmx", "tmx"]
+__all__ = ["load_tmx", "tmx", "header", "tu", "tuv"]
