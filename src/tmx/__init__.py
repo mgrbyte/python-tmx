1b43533057dd9f0aa9419f11476f240d90e8cf98
Matt Russell <m.russell@bangor.ac.uk>
2024-03-28 10:51:53 +0000

    Expose all structural classes.

--- src/tmx/__init__.py
+++ src/tmx/__init__.py
@@ -1,4 +1,4 @@
 from .loaders import load_tmx
-from .structural import tmx, header, tu, tuv
+from .structural import tmx, header, note, prop, run, tu, tuv
 
-__all__ = ["load_tmx", "tmx", "header", "tu", "tuv"]
+__all__ = ["load_tmx", "tmx", "header", "note", "prop", "run", "tu", "tuv"]
